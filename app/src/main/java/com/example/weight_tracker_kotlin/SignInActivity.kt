498623382a7e399a0961ae87979ee8d52ef9f6e9
Joni Tiainen <jonitia91@gmail.com>
2023-02-14 19:20:23 +0200

    Added: sign in work with firebase authentication. Next is to fix registration

--- app/src/main/java/com/example/weight_tracker_kotlin/SignInActivity.kt
+++ app/src/main/java/com/example/weight_tracker_kotlin/SignInActivity.kt
@@ -1,11 +1,61 @@
 package com.example.weight_tracker_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        auth = FirebaseAuth.getInstance()
+
+        // sign in button in sign in activity
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+
+        btnSignIn.setOnClickListener {
+            try {
+                val signInUsernameText = findViewById<EditText>(R.id.signInUsernameText)
+                val signInPasswordText = findViewById<EditText>(R.id.signInPasswordText)
+
+                auth.signInWithEmailAndPassword(
+                    signInUsernameText.text.toString(),
+                    signInPasswordText.text.toString()
+                )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success
+                            // print user info email, uid if exists
+                            println("Signed in successfully")
+                            println("User info: ${auth.currentUser?.email}")
+                            println("User info: ${auth.currentUser?.uid}")
+                            println("User info: ${auth.currentUser}")
+
+                            // go to main activity
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            // If sign in fails
+                            println("Error: ${task.exception?.message}")
+                        }
+                        // sign out user after handling the task results to prevent data leaks
+                        auth.signOut()
+                    }
+            } catch (e: Exception) {
+                println("Error: ${e.message}")
+            } finally {
+                println("Finished")
+                // close sign in activity by signing out regardless of success or failure
+                // this will prevent data leaks
+                auth.signOut()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/weight_tracker_kotlin/SignUpActivity.kt
+++ app/src/main/java/com/example/weight_tracker_kotlin/SignUpActivity.kt
@@ -7,5 +7,11 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        //TODO: 1. add sign up functionality
+        //TODO: 2. Create main activity, statistic activity and account layout activity
+        //TODO: 3. Create logout in account activity
+        //TODO: 4. Create weight entry in main activity
+        //TODO: 5. Create weight summary in statistic activity
     }
 }
