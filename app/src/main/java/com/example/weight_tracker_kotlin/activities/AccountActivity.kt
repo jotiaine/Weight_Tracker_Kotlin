4221318962ee248df27faa5719a400aa43c19d6d
Joni Tiainen <jonitia91@gmail.com>
2023-02-17 16:59:34 +0200

    Added: numeral input fragment - not ready

--- app/src/main/java/com/example/weight_tracker_kotlin/activities/AccountActivity.kt
+++ app/src/main/java/com/example/weight_tracker_kotlin/activities/AccountActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weight_tracker_kotlin.R
 
-class MainActivity : AppCompatActivity() {
+class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_account)
     }
 }
