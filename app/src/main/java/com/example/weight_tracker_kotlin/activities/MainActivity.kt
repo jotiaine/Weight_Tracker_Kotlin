99a3564152a58be253ddd519f3ffeac0d58e390e
Joni Tiainen <jonitia91@gmail.com>
2023-02-19 19:58:59 +0200

    Added: home, stats and account fragments. Logout is ready, add functionality next

--- app/src/main/java/com/example/weight_tracker_kotlin/activities/MainActivity.kt
+++ app/src/main/java/com/example/weight_tracker_kotlin/activities/MainActivity.kt
@@ -3,30 +3,59 @@ package com.example.weight_tracker_kotlin.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.weight_tracker_kotlin.R
+import com.example.weight_tracker_kotlin.fragments.AccountFragment
+import com.example.weight_tracker_kotlin.fragments.HomeFragment
+import com.example.weight_tracker_kotlin.fragments.StatisticFragment
+import com.google.android.gms.dynamic.SupportFragmentWrapper
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var frameLayout: FrameLayout
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var statisticFragment: StatisticFragment
+    private lateinit var accountFragment: AccountFragment
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var fragmentTransaction: FragmentTransaction
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("MainActivity")
+
+        frameLayout = findViewById(R.id.frame_layout)
+
+        fragmentManager = supportFragmentManager
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
 
-
-        bottomNavigationView.setOnItemSelectedListener  { item ->
+        bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home -> {
                     // Already on the home destination
+                    homeFragment = HomeFragment()
+                    fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.frame_layout, homeFragment)
+                    fragmentTransaction.commit()
                     true
                 }
                 R.id.stats -> {
+                    statisticFragment = StatisticFragment()
+                    fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.frame_layout, statisticFragment)
+                    fragmentTransaction.commit()
                     true
                 }
                 R.id.account -> {
+                    accountFragment = AccountFragment()
+                    fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.frame_layout, accountFragment)
+                    fragmentTransaction.commit()
                     true
                 }
                 else -> false
